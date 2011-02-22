8bf82976bdbd4c5097f5490e422f300e4d79d2c5
felipe <felipe@tzi.de>
2011-02-22 10:55:07 +0100

    Helpers für listen

--- courseManagement/app/helpers/groups_helper.rb
+++ courseManagement/app/helpers/groups_helper.rb
@@ -1,2 +1,12 @@
 module GroupsHelper
+  def get_available_groups()
+    result = Array.new
+    Group.all.each do |group|
+      if group.users == nil || group.users.length<2
+        result.push(group)
+      end
+    end
+
+    return result
+  end
 end
--- courseManagement/app/helpers/projects_helper.rb
+++ courseManagement/app/helpers/projects_helper.rb
@@ -1,11 +1,12 @@
 module ProjectsHelper
-  def get_available_projects()
+  def get_available_projects(group)
     result = Array.new
     Project.all.each do |project|
-      if project.group == nil
-        result.push(group)
+      if project.group == nil || project.group==group
+        result.push(project)
       end
     end
+
     return result
   end
 end
