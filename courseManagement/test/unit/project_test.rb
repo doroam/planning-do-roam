f27c583fe5573bc6fc8ee3c3b5d246799d49f785
Oliver Schnieders <maximus@tzi.de>
2011-02-22 13:34:11 +0100

    Testfälle

--- courseManagement/test/unit/project_test.rb
+++ courseManagement/test/unit/project_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def test_should_create_project
+    project = Project.new(:name => "TestProject")
+    assert project.save
+  end
+  
+  def test_should_have_2_projects
+    assert_equal 2, Project.count
   end
 end
