9cd2b5fe545dec9a33e39656dc80ace96cb1ebc5
felipe <felipe@tzi.de>
2011-02-25 20:14:04 +0100

    alerts out

--- ActivityRoutePlanner/app/controllers/init_controller.rb
+++ ActivityRoutePlanner/app/controllers/init_controller.rb
@@ -10,7 +10,7 @@ class InitController < ApplicationController
       activity_list.push(Activity.new("amenity", "hospital"))
       activity_list.push(Activity.new("amenity", "bank"))
       activity_list.push(Activity.new("amenity", "police"))
-      activity_list.push(Activity.new("amenity", "telephone"))
+      activity_list.push(Activity.new("amenity", "restaurant"))
 
       session[:main_activity_list] = activity_list
       session[:main_route] = route
