9aab13d4c6611c2aa24394ebc42944b03c4b9bc6
Oliver Schnieders <maximus@tzi.de>
2011-02-26 14:18:50 +0100

    Löschen von Punkten

--- ActivityRoutePlanner/app/controllers/generate_route_controller.rb
+++ ActivityRoutePlanner/app/controllers/generate_route_controller.rb
@@ -1,17 +1,52 @@
 class GenerateRouteController < ApplicationController
   
-
   def updateRoute
     route = session[:main_route]
            
     if params[:start]!=nil
-      route.start_point.label = params[:start]
-      route.start_point.parse_label
+      route = setStartPoint(route, params[:start])
     elsif params[:end]!=nil
-      route.end_point.label = params[:end]
-      route.end_point.parse_label
+      route = setEndPoint(route, params[:end])
+    elsif params[:delete_point] != nil
+      if params[:delete_point].eql? "start"
+        route = removeStartPoint(route)
+      elsif params[:delete_point].eql? "end"  
+        route = removeEndPoint(route)   
+      end 
     end
+ 
+    route = activateOrDeactivateActivities(route) 
+
+    session[:main_route] = route
     
+    respond_to do |format|      
+      format.js 
+    end
+  end
+  
+  def removeStartPoint(route)
+    route.start_point = Point.new
+    return route
+  end
+  
+  def removeEndPoint(route)
+    route.end_point = Point.new
+    return route
+  end
+  
+  def setStartPoint(route, label)
+      route.start_point.label = label
+      route.start_point.parse_label      
+      return route
+  end
+  
+  def setEndPoint(route, label)
+      route.end_point.label = label
+      route.end_point.parse_label
+      return route
+  end
+  
+  def activateOrDeactivateActivities(route)
     #If user has given an start- and endPoint, 
     #so activates activity-list if not done
     if (route.start_point.label != nil) && (route.end_point.label != nil)
@@ -19,14 +54,14 @@ class GenerateRouteController < ApplicationController
         if route.activities == nil
            route.activities = Array.new
            route.activities.push(Activity.new("", ""))
-        end   
-      end       
+        end
+      else
+        route.activities = nil
+      end     
+    else
+      route.activities = nil
     end
-
-    session[:main_route] = route
     
-    respond_to do |format|      
-      format.js 
-    end
+    return route
   end
 end
