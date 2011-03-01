a4dd91f8f3f2c40567755cde9d54d284f4d432ec
felipe <felipe@tzi.de>
2011-03-01 15:51:52 +0100

    doc

--- ActivityRoutePlanner/app/models/route.rb
+++ ActivityRoutePlanner/app/models/route.rb
@@ -1,20 +1,30 @@
 class Route 
-  
+  GLOBAL_FIELD_NAME     = Global::GLOBAL_FIELD_NAME
+  GLOBAL_FIELD_LONG     = Global::GLOBAL_FIELD_LONG
+  GLOBAL_FIELD_LAT      = Global::GLOBAL_FIELD_LAT
+  GLOBAL_TABLE_POINT    = Global::GLOBAL_TABLE_POINT
+  GLOBAL_TABLE_POLYGON  = Global::GLOBAL_TABLE_POLYGON
+  GLOBAL_FIELD_AMENITY  = Global::GLOBAL_FIELD_AMENITY
+
   attr_accessor :start_point,:end_point,:activities
   def initialize
     @start_point = Point.new
     @end_point = Point.new
   end
   
-
+  #Creates javascript code to show the Marks of the selected route
+  #TODO show path
   def show_markers()
     script = ""
+    #adds start mark
     if @start_point != nil && @start_point.label!=nil
       script = "addMark('"+@start_point.label+"','"+@start_point.lat+"','"+@start_point.long+"','start');"
     end
+    #adds end mark
     if @end_point != nil && @end_point.label
       script += "addMark('"+@end_point.label+"','"+@end_point.lat+"','"+@end_point.long+"','end');"
     end
+    #adds activities mark
     if @activities != nil
       @activities.each.with_index do |activity,id|
         if activity.result != nil
@@ -25,53 +35,34 @@ class Route
       end
     end
     return script
-  end
-    
-  def self.get_closest_activities(route)        
-    pstart  = route.start_point
-    pend    = route.end_point
-    activities = route.activities
-    global_field_name = Global::global_field_name
-    global_field_long = Global::global_field_long
-    global_field_lat  = Global::global_field_lat
-    global_table_point = Global::global_table_point
-    global_table_polygon = Global::global_table_polygon
-    global_field_amenity = Global::global_field_amenity
-    sqlHeadPoint    = "select "+global_field_name+","+global_field_long+","+global_field_lat+","+get_distance_query(pstart,pend)+" from "+global_table_point
-    sqlHeadPolygon  = "select "+global_field_name+","+global_field_long+","+global_field_lat+","+get_distance_query(pstart,pend)+" from "+global_table_polygon
+  end    
 
-    activities.each do |activity|
-      limit   = " order by distance limit 3;"
-      where   = " where "+global_field_amenity+" = '"+activity.value+"' "
-      sql = "("+sqlHeadPoint+where+" union "+sqlHeadPolygon+where+") "+limit
-      result  = execute_sql(sql)
-      activity.result = result
-    end
-  end
-
+  #gets the 3 closest points to start and endpoint where the activity can be done
   def self.get_closest_activity(activity,pstart,pend)
-    global_field_name = Global::GLOBAL_FIELD_NAME
-    global_field_long = Global::GLOBAL_FIELD_LONG
-    global_field_lat  = Global::GLOBAL_FIELD_LAT
-    global_table_point = Global::GLOBAL_TABLE_POINT
-    global_table_polygon = Global::GLOBAL_TABLE_POLYGON
-    global_field_amenity = Global::GLOBAL_FIELD_AMENITY
-    sql_head_point    = "select "+global_field_name+","+global_field_long+","+global_field_lat+","+get_distance_query(pstart,pend)+" from "+global_table_point
-    sql_head_polygon  = "select "+global_field_name+","+global_field_long+","+global_field_lat+","+get_distance_query(pstart,pend)+" from "+global_table_polygon
+    
+    sql_head_point    = "select "+GLOBAL_FIELD_NAME+","+GLOBAL_FIELD_LONG+","+GLOBAL_FIELD_LAT+","+get_distance_query(pstart,pend)+" from "+GLOBAL_TABLE_POINT
+    sql_head_polygon  = "select "+GLOBAL_FIELD_NAME+","+GLOBAL_FIELD_LONG+","+GLOBAL_FIELD_LAT+","+get_distance_query(pstart,pend)+" from "+GLOBAL_TABLE_POLYGON
+    #order the results to get closest TODO limit 1!
     limit   = " order by distance limit 3;"
-    where   = " where "+global_field_amenity+" = '"+activity.value+"' "
+    where   = " where "+activity.tag+" = '"+activity.value+"' "
     sql     = "("+sql_head_point+where+" union "+sql_head_polygon+where+") "+limit
     result  = execute_sql(sql)
     activity.result = result
   end
 
   private
+  #Gets the distance from the point to the start point and from the point to the endpoint
+  #and adds them. So we can later sort this distance and find the nearest one
+  #TODO: Get path distance, not airline distance
   def self.get_distance_query(pstart,pend)
     distance_p1 = "distance(GeomFromText('POINT("+pstart.long+" "+pstart.lat+")',4326),st_transform(way,4326))"
     distance_p2 = "distance(GeomFromText('POINT("+pend.long+" "+pend.lat+")',4326),st_transform(way,4326))"
     result      = "("+distance_p1+"+"+distance_p2+")as distance"
     return result
   end
+
+
+  # executes a sql query and gets the results
   def self.execute_sql(sql)
     #sql = "SELECT name,X(transform(way,4326)),Y(transform(way,4326))  FROM planet_osm_point where name like '%bremen%';"
     ActiveRecord::Base.establish_connection(:osm_data)
@@ -79,6 +70,9 @@ class Route
     res = get_sql_results(res)
     return res
   end
+
+
+  #creates points from the results of a sql query
   def self.get_sql_results(res)
     result = Array.new
     res.each  do |row|
@@ -88,6 +82,9 @@ class Route
     end
     return result
   end
+
+
+  #creates a point from a result of the database
   def self.make_point(row)
     name  = row["name"]
     lat   = row["y"]
