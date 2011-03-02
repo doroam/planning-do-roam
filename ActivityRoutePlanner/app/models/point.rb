57720be2b6ec491f3493aaf4901b2979efe04aed
Oliver Schnieders <maximus@tzi.de>
2011-03-02 12:11:18 +0100

    Parsen des Labels für die Points

--- ActivityRoutePlanner/app/models/point.rb
+++ ActivityRoutePlanner/app/models/point.rb
@@ -16,6 +16,13 @@ class Point
     if results.size > 1
       @lat = results[0]
       @long = results[1]
+      
+      num_lat = @lat.to_f
+      num_lat = num_lat.round(8)
+      num_lon = @long.to_f
+      num_lon = num_lon.round(8)      
+      
+      @label = num_lat.to_s+";"+num_lon.to_s
     else
       #
     end
