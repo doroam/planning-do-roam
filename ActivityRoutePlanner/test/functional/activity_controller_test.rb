5e46ef5fce966060595ceafca34024342dac61ca
Oliver Schnieders <maximus@tzi.de>
2011-03-03 13:42:05 +0100

    Controller Tests entfernt

--- ActivityRoutePlanner/test/functional/activity_controller_test.rb
+++ ActivityRoutePlanner/test/functional/activity_controller_test.rb
@@ -2,26 +2,5 @@ require 'test_helper'
 
 class ActivityControllerTest < ActionController::TestCase
   
-  test "ajax redirect test" do
 
-  end
-  
-  test "delete activity" do
-    act = valid_activity_list
-    assert_difference(act.count, -1) do
-      
-    end
-  end
-  
-  test "add activity" do
-    
-  end
-  
-  test "change activity" do
-    
-  end
-  
-  test "create activity" do
-    
-  end
 end
