b7b19de06fbebbb308f1ec7565f72c216f8731f0
Mihai Codescu <Mihai.Codescu@dfki.de>
2012-06-28 12:50:41 +0200

    made localisation worj

--- ActivityRoutePlanner/app/controllers/application_controller.rb
+++ ActivityRoutePlanner/app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+before_filter :set_locale
+ 
+def set_locale
+  I18n.locale = params[:locale] || I18n.default_locale
+end
   
 private 
   #get the current route of the session
