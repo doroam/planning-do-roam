561f1d785bdee90d63155cc0f1a926802176cff5
militaru <militaru@militaru-laptop.(none)>
2012-07-26 15:48:28 +0200

    Complete View

--- ActivityRoutePlanner/app/controllers/display_controller.rb
+++ ActivityRoutePlanner/app/controllers/display_controller.rb
@@ -1,24 +1,11 @@
 class DisplayController < ApplicationController
   def index
     @user=Testuser.all
-    #@data=Testdata.all
-    
-    #@result=Hash.new
-    #@user.each do |users|
-     # a=Array.new
-      #@data.each do |datas|
-	#if datas.testuser=users.id
-	 # a.push(datas)
-	#end
-	
-      #end
-      #@result.merge!({users=>a})
-    #end
     
   end
   
   def black
-    
+    @user2=Testuser.find_by_id(params[:param])
     @data=Testdata.find_all_by_testuser(params[:param])   
   end
     
